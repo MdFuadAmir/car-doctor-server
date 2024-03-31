ebe156fcd25d81502ed626a357a5528997df111c
Md Fuad Amir <mdfuadamir@gmail.com>
2024-03-31 21:35:19 +0600

    added some commit

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ async function run() {
       res.send(result);
     })
 
+
+
+
+    
+
     app.get('/services/:id', async(req,res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
